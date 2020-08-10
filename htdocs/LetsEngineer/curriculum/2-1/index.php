985cac80c8aacaa2a8941ddd9355b3fd27114811
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-10 20:16:08 +0900

    Hello World 課題

---
+++ htdocs/LetsEngineer/curriculum/2-1/index.php
@@ -0,0 +1,22 @@
+<?php
+// PHPの研修カリキュラム2-1
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-1
+*/
+?>
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+<?php
+echo "1.開発環境・検証環境・本番環境"."<br>";
+echo " 開発環境：WEBシステムなどの開発段階で用いる環境　検証環境：開発をし終え、リリース時の動作確認などを行うための環境　本番環境；普段私たちが使用しているWEBサービスやアプリケーションなどがおかれている環境"."<br>";
+echo "2.基本設計書・詳細設計書"."<br>";
+echo "基本設計書：ソフトウェアの動きを外から見た際にどういう動きをするのか？(What)を決めたもの　詳細設計書：基本設計で決められた動きをどうやって実現するか？(How)を決めたもの"."<br>";
+echo "3.Ajax"."<br>";
+echo "JavaScriptとXMLを使って非同期にサーバとの間の通信を行うこと。"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
