20b7ba6bf7dac6ca3a175f50d9c4dd383d08d258
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-10 21:07:02 +0900

    変数とデータ型　課題

---
+++ htdocs/LetsEngineer/curriculum/2－2/index.php
@@ -0,0 +1,20 @@
+<?php
+// PHPの研修カリキュラム2-1
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-1
+*/
+?>
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+<?php
+echo "1.API"."<br>";
+echo " API:　あるコンピュータプログラム（ソフトウェア）の機能や管理するデータなどを、外部の他のプログラムから呼び出して利用するための手順やデータ形式などを定めた規約のこと。"."<br>";
+echo "2. SVN"."<br>";
+echo "SVN：、オープンソースソフトウェアとして提供されているバージョン管理システムの名称である。"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
